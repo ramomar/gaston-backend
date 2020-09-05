36cae0d8ace76296be1784bae599c198320b19f9
Eduardo Garza <omaarg@gmail.com>
2020-09-05 16:55:40 -0500

    Add post review fn

---
+++ functions/review.py
@@ -0,0 +1,61 @@
+import os
+import json
+import boto3
+from decimal import Decimal
+import botocore.exceptions
+
+IS_AWS = os.environ.get('AWS_EXECUTION_ENV', False)
+OWNER_ID = os.environ.get('OWNER_ID', 'ramomar')
+
+
+def post_review(event, context):
+    client = boto3.resource('dynamodb', **{} if IS_AWS else {'endpoint_url': 'http://localhost:8000'})
+    table = client.Table('gaston' if IS_AWS else 'gaston-local')
+
+    if 'review' not in event:
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Content-Type': 'application/json',
+            },
+            'body': {
+                'code': 'NoReview',
+                'message': 'Review is not present'
+            },
+        }
+
+    try:
+        review = dict(event['review'], **{'amount': Decimal(event['review']['amount'])}) if 'amount' in event else event['review']
+
+        update_result = table.update_item(
+            Key={'owner_id': OWNER_ID, 'record_id': event['record_id']},
+            ReturnValues='ALL_NEW',
+            UpdateExpression='SET review = :review',
+            ExpressionAttributeValues={
+                ':review': review,
+            },
+            ConditionExpression='attribute_exists(record_id)'
+        )
+        result = {
+            'review': update_result['Attributes'],
+        }
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+            },
+            'body': json.dumps(result, indent=4, default=str),
+        }
+    except botocore.exceptions.ClientError as error:
+        error_details = error.response['Error']
+
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Content-Type': 'application/json',
+            },
+            'body': {
+                'code': error_details['Code'],
+                'message': error_details['Message'],
+            },
+        }
---
+++ tests/test_review.py
@@ -0,0 +1,125 @@
+import uuid
+import json
+from decimal import Decimal
+from functions import review
+
+
+def test_post_review_record_not_found():
+    """it should fail when trying to update a record that doesn't exist"""
+    event = {
+        'record_id': str(uuid.uuid4()),
+        'review': {
+            'amount': 150,
+            'date': '2017-03-19T05:29:02.700Z',
+            'note': 'Cena',
+            'category': 'Comida'
+        },
+    }
+    actual = review.post_review(event, context=None)
+    expected = {
+        'statusCode': 400,
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+        'body': {
+            'code': 'ConditionalCheckFailedException',
+            'message': 'The conditional request failed',
+        }
+    }
+
+    assert actual == expected
+
+
+def test_post_review_no_review():
+    """it should fail if there is no review"""
+    event = {
+        'record_id': str(uuid.uuid4()),
+    }
+    actual = review.post_review(event, context=None)
+    expected = {
+        'statusCode': 400,
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+        'body': {
+            'code': 'NoReview',
+            'message': 'Review is not present',
+        }
+    }
+
+    assert actual == expected
+
+
+def test_post_review_record_found(gaston_table):
+    """it should update the record"""
+    record_raw = {
+        'source': 'FAST_TRANSFER_EMAIL',
+        'type': 'EXPENSE',
+        'note': 'Transferencias Rápidas | P',
+        'amount': '650',
+        'operation_date': '27/Jul/2020 18:56:55 horas',
+        'application_date': None,
+        'receiver': {
+            'name': 'No capturado',
+            'bank': 'BANCO'
+        },
+        'channel': None,
+        'extra_amounts': [
+            {
+                'name': 'fee',
+                'amount': '3.00',
+                'tax': '0.48',
+            }
+        ],
+    }
+    item_id = str(uuid.uuid4())
+    item = {
+        'owner_id': 'ramomar',
+        'record_id': item_id,
+        'note': record_raw['note'],
+        'amount': Decimal(record_raw['amount']),
+        'date': record_raw['operation_date'],
+        'raw': json.dumps(record_raw, default=str, ensure_ascii=False),
+        'origin': 'BANORTE_EMAIL_SES',
+    }
+
+    gaston_table.put_item(Item=item)
+
+    event = {
+        'record_id': item_id,
+        'review': {
+            'amount': 655,
+            'date': '2020-07-19T18:56:00.000Z',
+            'note': 'Curso de musica',
+            'category': 'Educación'
+        },
+    }
+    actual = review.post_review(event, context=None)
+    expected_review = {
+        'owner_id': 'ramomar',
+        'record_id': item_id,
+        'note': record_raw['note'],
+        'amount': record_raw['amount'],
+        'date': record_raw['operation_date'],
+        'raw': json.dumps(record_raw, default=str, ensure_ascii=False),
+        'origin': 'BANORTE_EMAIL_SES',
+        'review': {
+            'amount': '655',
+            'date': '2020-07-19T18:56:00.000Z',
+            'note': 'Curso de musica',
+            'category': 'Educación'
+        },
+    }
+    expected = {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+        'body': {
+            'review': json.dumps(expected_review, indent=4, default=str),
+        }
+    }
+
+    assert actual['statusCode'] == expected['statusCode']
+    assert actual['headers'] == expected['headers']
+    assert json.loads(actual['body'])['review'] == json.loads(expected['body']['review'])
