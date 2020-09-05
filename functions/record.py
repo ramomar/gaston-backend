63d5ea2eebdebd8fdf135751ca3ca620d0cd3651
Eduardo Garza <omaarg@gmail.com>
2020-09-05 16:09:45 -0500

    Rename variable

--- functions/record.py
+++ functions/record.py
@@ -27,14 +27,14 @@ def get_records(event, context):
 def get_record(event, context):
     client = boto3.resource('dynamodb', **{} if IS_AWS else {'endpoint_url': 'http://localhost:8000'})
     table = client.Table('gaston' if IS_AWS else 'gaston-local')
-    item = table.get_item(Key={'owner_id': OWNER_ID, 'record_id': event['record_id']})
+    get_item_result = table.get_item(Key={'owner_id': OWNER_ID, 'record_id': event['record_id']})
 
     result = {
-        'record': item['Item'] if 'Item' in item else None
+        'record': get_item_result['Item'] if 'Item' in get_item_result else None
     }
 
     return {
-        'statusCode': 200 if 'Item' in item else 404,
+        'statusCode': 200 if 'Item' in get_item_result else 404,
         'headers': {
             'Content-Type': 'application/json',
         },
