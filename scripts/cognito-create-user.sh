1b27891079e89894a2884356b24c41001055dcf1
eduardo <ramomar@users.noreply.github.com>
2020-09-08 20:47:57 -0500

    Update cognito-create-user.sh

--- scripts/cognito-create-user.sh
+++ scripts/cognito-create-user.sh
@@ -3,4 +3,4 @@ cognito_username=
 cognito_password=
 
 aws cognito-idp admin-create-user --user-pool-id $cognito_userpool_id --cognito_username $cognito_username
-aws cognito-idp admin-set-user-password --user-pool-id $cognito_userpool_id --cognito_username $cognito_username --password $cognito_password
+aws cognito-idp admin-set-user-password --user-pool-id $cognito_userpool_id --username $cognito_username --password $cognito_password
